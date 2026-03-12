5a06d3af6dd73021ebc1f0d7cc62993f97f26ff8
Patricia Fricke <patricia.fricke@noaa.gov>
2026-03-12 04:28:15 +0000

    manually add prefix_path to openapi_url in app.py

--- stac_fastapi/pgstac/app.py
+++ stac_fastapi/pgstac/app.py
@@ -162,7 +162,7 @@ async def lifespan(app: FastAPI):
 
 api = StacApi(
     app=FastAPI(
-        openapi_url=settings.openapi_url,
+        openapi_url=settings.prefix_path + settings.openapi_url,
         docs_url=settings.prefix_path + settings.docs_url,
         redoc_url=None,
         root_path=settings.root_path,
